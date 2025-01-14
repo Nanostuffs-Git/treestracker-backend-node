222fafe4bb4e683f3d0d553777bd0d67388bab1a
vishwaRahane <www.abcom.in@wwwabcomins-MacBook-Air.local>
2025-01-15 01:24:28 +0530

    feat: changes in login api

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,28 +1,36 @@
 const db = require("../models/db");
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, mobileNo } = req.body;
 
-    if (!username || !password) {
-        return res.status(400).json({ message: 'Username and password are required.' });
+    if (!mobileNo && (!username || !password)) {
+        return res.status(400).json({ message: 'Invalid login details. Please provide valid credentials.' });
     }
 
     try {
-        const query = "SELECT * FROM users WHERE username = $1";
-        const values = [username];
+        let query, values;
+
+        if (mobileNo) {
+            query = "SELECT * FROM users WHERE mobile_no = $1";
+            values = [mobileNo];
+        } else {
+            query = "SELECT * FROM users WHERE username = $1";
+            values = [username];
+        }
+
         const result = await db.query(query, values);
         const user = result?.rows[0];
 
         if (result?.rows?.length === 0) {
-            return res.status(401).json({ message: 'User not found' });
+            if (mobileNo) {
+                return res.status(401).json({ message: 'Mobile number not found.' });
+            } else {
+                return res.status(401).json({ message: 'Username not found.' });
+            }
         }
-        // Check if the user exists
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid username or password.' });
-        }
-        // Directly compare the provided password with the stored password
-        if (password !== user.password_hash) {
-            return res.status(401).json({ message: 'Invalid username or password.' });
+
+        if (!mobileNo && password !== user.password_hash) {
+            return res.status(401).json({ message: 'Invalid username or password. Please try again.' });
         }
 
         const userData = {
@@ -34,13 +42,12 @@ exports.login = async (req, res) => {
             lastname: user?.lastname,
         };
 
-        // Send success response
         return res.status(200).json({
             success: true,
             user: userData,
         });
     } catch (error) {
         console.error('Error during login:', error);
-        return res.status(500).json({ message: 'An error occurred during login.' });
+        return res.status(500).json({ message: 'An error occurred during login. Please try again later.' });
     }
 };
