83feb8308719f00fa6ed1af3b58774f499173ea6
vishwaRahane <www.abcom.in@wwwabcomins-MacBook-Air.local>
2025-01-15 00:18:14 +0530

    feat: changes in qr code api

--- src/controllers/myQrCodesController.js
+++ src/controllers/myQrCodesController.js
@@ -8,22 +8,37 @@ exports.getQrCodes = async (req, res) => {
     }
 
     try {
-        const query = `   
-         SELECT qc.* 
-         FROM qr_codes qc
-         JOIN users u ON qc.user_id = u.user_id
-         WHERE u.username = $1;
-          `;
+        const query = `
+            SELECT 
+                qc.qrcode_id,
+                qc.tree_id,
+                qc.user_id,
+                u.firstname,
+                u.lastname,
+                ts.latitude,
+                ts.longitude,
+                ts.photo_url,
+                ts.plantation_type,
+                ts.scientific_name,
+                ts.local_name,
+                ts.tree_condition,
+                ts.plant_type,
+                ts.remarks
+            FROM qr_codes qc
+            JOIN users u ON qc.user_id = u.user_id
+            LEFT JOIN tree_surveys ts ON qc.tree_id = ts.tree_id
+            WHERE u.username = $1;
+        `;
         const values = [username];
         const result = await db.query(query, values);
 
         if (result?.rows?.length === 0) {
-            return res.status(404).json({ message: 'No qr codes found for this user.' });
+            return res.status(404).json({ message: 'No QR codes found for this user.' });
         }
 
         return res.status(200).json(result.rows);
     } catch (error) {
-        console.error('Error fetching tree surveys:', error);
+        console.error('Error fetching QR codes:', error);
         return res.status(500).json({ message: 'Server error. Please try again later.' });
     }
 };
